18b215007015e55f648c577e41151053abfead2c
nhatxtn <quangnhat26092001@gmail.com>
2024-09-20 00:21:23 +0700

    feat-G5 (ui): view artwork detail for manga

--- src/components/artwork/artwork3Page/DescriptionSection.vue
+++ src/components/artwork/artwork3Page/DescriptionSection.vue
@@ -2,19 +2,20 @@
     <q-card>
         <div class="row">
             <div class="text-title">{{ title }}</div>
-            <q-icon class="text-center q-py-sm q-px-sm rotate-135" name="ion-link"></q-icon>
+            <q-icon class="text-center q-py-sm q-px-sm rotate-135" name="ion-link"
+                :style="{ boxShadow: '0 0 5px 2px #F9FAFC', borderRadius: '50%' }"></q-icon>
         </div>
         <div class="description q-py-xs q-pt-sm">
-            <div class="row">
+            <div class="row text-description">
                 <div class="text-country col-6 q-pt-xs">
                     <strong>Quốc gia:</strong> {{ country }}
                 </div>
-                <div class="text-status col-6">
+                <div class="text-status col-6 ">
                     <strong>Trạng thái: </strong> {{ status }}
-                    <q-icon class="q-px-xs" color=green name="ion-checkmark-circle"></q-icon>
+                    <q-icon class="q-px-xs" color="green" name="ion-checkmark-circle"></q-icon>
                 </div>
             </div>
-            <div class="row">
+            <div class="row text-description">
                 <div class="text-subtitle col-6 q-pt-xs">
                     <strong>Tác giả: </strong> {{ author }}
                 </div>
@@ -22,12 +23,34 @@
                     <strong>Series: </strong> {{ series }}
                 </div>
             </div>
-            <div class="row q-py-sm">
-                <q-btn v-for="(button, index) in buttons" :key="index" unelevated :style="{
-                    backgroundColor: '#1e3a8a',   // Dark blue color
-                    color: 'white',
-                    // White text
-                }" :label="button.label" class="q-mr-md"></q-btn>
+            <div class="row q-py-xs">
+                <q-badge v-for="(button, index) in buttons" :key="index" unelevated :style="{
+                    backgroundColor: '#120E36', // Dark blue color
+                    color: 'white', // Make text bold
+                    fontSize: '1rem', // Decrease font size
+                    padding: '0.4rem 0.3rem' // Adjust padding for a smaller badge
+                }" :label="button.label" class="q-mr-sm justify-center"></q-badge>
+            </div>
+            <div class="row absolute-bottom q-pb-md">
+                <q-btn class="col-2 q-mr-sm" unelevated rounded no-caps
+                    :style="{ fontSize: '0.7rem', backgroundColor: '#120E36', color: 'white', fontWeight: 'bold' }">
+                    <q-icon name="ion-arrow-dropright" class=" q-mr-xs" />
+                    Xem tiếp
+                </q-btn>
+                <q-btn class="col-2 q-mr-sm" unelevated rounded no-caps
+                    :style="{ fontSize: '0.7rem', backgroundColor: '#120E36', color: 'white', fontWeight: 'bold' }">
+                    Xem từ đầu
+                </q-btn>
+                <q-btn class="col-2 q-mr-sm" unelevated rounded no-caps
+                    :style="{ fontSize: '0.7rem', backgroundColor: '#EEEEEE', color: '#120E36', fontWeight: 'bold' }">
+                    <q-icon :color="'#120E36'" name="ion-heart" class="q-mr-xs" />
+                    Yêu thích
+                </q-btn>
+                <q-btn class="col-3 q-mr-sm" unelevated rounded no-caps
+                    :style="{ fontSize: '0.7rem', backgroundColor: '#EEEEEE', color: '#120E36', fontWeight: 'bold' }">
+                    <q-icon :color="'#120E36'" name="ion-alert" class="q-mr-xs" />
+                    Báo cáo vi phạm
+                </q-btn>
             </div>
         </div>
     </q-card>
@@ -47,9 +70,8 @@ const buttons = ref([
     { label: 'Phiêu lưu' },
     { label: 'Viễn tưởng' },
     { label: 'Hài hước' },
-    { label: 'đời thường' }
+    { label: 'Đời thường' }
 ])
-
 </script>
 
 <style scoped>
@@ -58,12 +80,13 @@ const buttons = ref([
 }
 
 .text-title {
-    font-size: 1.5em;
+    font-size: 1.5rem;
     font-weight: bold;
 }
 
 
-.desciption>div {
-    font-size: 18px;
+
+.text-description {
+    font-size: 0.8rem;
 }
 </style>
