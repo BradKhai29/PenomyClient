91cf02951e2eae7acfae138b96cbf5bb958b147b
poollynight <chienthang410@gmail.com>
2024-10-14 21:16:22 +0700

    push thangvvc-g52

--- src/pages/artwork/Artwork1Page.vue
+++ src/pages/artwork/Artwork1Page.vue
@@ -14,7 +14,6 @@ import UserComment from "src/components/common/artwork/UserComment.vue";
 import RecentlyUpdated from "src/components/common/artwork/comic/RecentlyUpdated.vue";
 import RecommendArtworkByCategory from "src/components/common/artwork/comic/ArtworkByCategory.vue";
 import CommentLoader from "src/components/common/artwork/CommentLoader.vue";
-import { useAuthStore } from "src/stores/common/AuthStore";
 import { ref } from "vue";
 
 
@@ -28,29 +27,3 @@ defineOptions({
 });
 </script>
 
-<template>
-    <q-page class="">
-        <span class="text-h1">{{ emoji }}</span>
-    </q-page>
-</template>
-
-<script setup>
-import { useAuthStore } from "src/stores/common/AuthStore";
-import { ref } from "vue";
-
-const authStore = useAuthStore();
-
-const emoji = ref("😭");
-
-const counterInitValue = 0;
-const counter = ref(counterInitValue);
-
-function clickMe() {
-    authStore.increment();
-    counter.value = authStore.counter;
-}
-
-defineOptions({
-    name: "IndexPage",
-});
-</script>
