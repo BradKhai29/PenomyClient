c988fd2d1c84377a216a04f60ff79fdb179c46dc
poollynight <chienthang410@gmail.com>
2024-10-23 21:40:04 +0700

    Like comment function-Change to facebook style

--- src/components/common/artwork/EmojiPickerBoard.vue
+++ src/components/common/artwork/EmojiPickerBoard.vue
@@ -1,46 +1,47 @@
 <template>
-    <div>
-        <EmojiPicker :native="true" :pickerType="'native'" :disable-sticky-group-names="true" :disableSkinTones="true"
-            @select="onSelectEmoji" />
+    <div class="row">
+        <Picker :data="emojiIndex" set="facebook" @select="onSelectEmoji" :showPreview="false" />
+    </div>
+
+    <div class="row">
+        <div>
+            {{ emojisOutput }}
+        </div>
     </div>
 </template>
 
 <script>
-import EmojiPicker from "vue3-emoji-picker";
-import "vue3-emoji-picker/css";
+import data from "emoji-mart-vue-fast/data/all.json";
+import "emoji-mart-vue-fast/css/emoji-mart.css";
+import { Picker, EmojiIndex } from "emoji-mart-vue-fast/src";
+
+let emojiIndex = new EmojiIndex(data);
+
 export default {
-    name: "HelloWorld",
-    props: {
-        msg: String,
-    },
+    name: "App",
     components: {
-        EmojiPicker,
+        Picker
     },
+
+    data() {
+        return {
+            emojiIndex: emojiIndex,
+            emojisOutput: ""
+        };
+    },
+
     methods: {
+        showEmoji(emoji) {
+            this.emojisOutput = this.emojisOutput + emoji.native;
+            console.log(emoji);
+        },
         onSelectEmoji(e) {
-            this.$emit("onIconSelected", e.i);
+            this.$emit("onIconSelected", e.native);
         }
-    },
+    }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
-}
+<style>
 
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
-}
 </style>
