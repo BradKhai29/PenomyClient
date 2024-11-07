e11799fc5cd271ec2c816f0abcc4f55ab89fdff9
poollynight <chienthang410@gmail.com>
2024-11-07 22:25:34 +0700

    HOTFIX- Get comment by tags

--- src/components/common/artwork/Common/CommentLoader.vue
+++ src/components/common/artwork/Common/CommentLoader.vue
@@ -1,5 +1,21 @@
 <template>
     <comment-input-field :artworkId="props.artworkId" @createComment="onCreateComment" />
+    <q-tabs v-model="commentSection" class="text-primary text-h6 navigation q-pl-lg" no-caps inline-label dense
+        max-width="300px" indicator-color="dark">
+        <q-tab name="1" @click="getComments()">
+            <div>
+                <q-icon name="comment" size="sm" class="text-primary" />
+                <span class="text-black"> Bình luận ({{ comments.length }})</span>
+            </div>
+        </q-tab>
+        <q-tab name="2" @click="getComments()">
+            <span class="text-black">Top</span>
+        </q-tab>
+        <q-tab name="3" @click="getComments()">
+            <span class="text-black">Mới nhất</span>
+        </q-tab>
+        <q-space />
+    </q-tabs>
     <h5 class="no-comment" v-if="comments.length === 0">No comments</h5>
     <UserComment v-for="comment in comments" :key="comment.id" :comment="comment" @deleteComment='onCommentDelete'
         @replyComment='onReplyCommentCreate' @replyCommentDelete='onReplyCommentDelete' />
@@ -11,14 +27,15 @@ import axios from "axios";
 import { BaseWebApiUrl } from "src/api.common/BaseWebApiUrl";
 import { HttpMethod } from "src/api.common/HttpMethod";
 import UserComment from "./UserComment.vue";
-import CommentInputField from "./CommentInputField.vue";
-import { useArtwork3Store } from 'src/stores/pages/artwork3/Artwork3Store';
+import CommentInputField from "./CommentInputField.vue"; 3
+// import { useArtwork3Store } from 'src/stores/pages/artwork3/Artwork3Store';
 import { useAuthStore } from "src/stores/common/AuthStore";
 
 var comments = ref([]);
 const apiUrl = `${BaseWebApiUrl}/g10/ArtworkComment/get`;
-const store = useArtwork3Store();
+// const store = useArtwork3Store();
 const authStore = useAuthStore();
+const commentSection = ref("1");
 
 var props = defineProps({
     artworkId: {
@@ -30,11 +47,6 @@ onMounted(() => {
     getComments()
 })
 
-watch(() => store.target, () => {
-    console.log("store change")
-    comments.value.filter((comment) => comment.id == store.commentId).totalReplies = store.getTotalReply
-})
-
 async function getComments() {
     if (authStore.isAuth) {
         await axios({
@@ -42,6 +54,7 @@ async function getComments() {
             method: HttpMethod.GET,
             params: {
                 artworkId: props.artworkId,
+                commentSection: commentSection.value
             },
             headers: {
                 Authorization: authStore.bearerAccessToken
@@ -84,5 +97,14 @@ function onReplyCommentDelete(parentCommentId) {
 <style scoped>
 .no-comment {
     margin-left: 260px;
+}
+
+.navigation {
+    /* margin: 0.5rem; */
+    align-self: center;
+    /* width: 70%; */
+    border-bottom: solid 0.1px grey;
+    margin: 2rem 260px 0 260px;
+
 }
 </style>
